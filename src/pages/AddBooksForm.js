cf48ca4e48f0eb366e2d37db8faa463c7675cef4
RNyaupane <roshan.nyaupane@apexcollege.edu.np>
2023-06-20 10:51:43 +0545

    path corrected

--- src/pages/AddBooksForm.js
+++ src/pages/AddBooksForm.js
@@ -1,48 +1,72 @@
-import React from 'react'
+import React, { useState } from 'react'
+import axios from 'axios'
 
 const AddBooksForm = () => {
-    
+
+    const bookFormSubmitHandler = (event) =>{
+        event.preventDefault();
+        const selectedBookName = event.target.bookName.value;
+        const selectedBookPrice = event.target.bookPrice.value;
+        const selectedBookType = event.target.bookType.value;
+        const selectedBookAuthor = event.target.bookAuthor.value;
+        const selectedBookQuantity = event.target.bookQuantity.value;
+        // const selectedBookPhoto = event.target.bookPhoto.files[0];
+
+        console.log(selectedBookQuantity)
+
+        // axios.post("",{selectedBookName,selectedBookPrice,selectedBookType,selectedBookAuthor,selectedBookQuantity})
+        // .then((Response) => {
+        //     console.log(Response)
+        // })
+        // .catch((error)=>{
+        //     console.log(error);
+        // })
+    }
+
+    const options = ['All', 'Book', 'Notes', 'Solution', 'Other Material'];
+
     return (
         <main id="main" className="main ">
             <section className="section dashboard">
                 <div className="row bg-light shadow p-3 mb-5 bg-body rounded mt-4 w-lg-75">
-
                     <div className="container-xxl py-5 wow fadeInUp" data-wow-delay="0.1s">
                         <div className="container">
                             <div className="row gy-5 gx-4">
                                 <h1 className="m-0 p-0 text-center">Post Ad</h1>
                                 <hr className="m-2 p-0" />
-                                <form className="row g-3">
+                                <form className="row g-3" onSubmit={bookFormSubmitHandler}>
                                     <div className="col-md-5">
                                         <label htmlFor="bookName" className="form-label">Name of the book <span className="text-danger">*</span></label>
-                                        <input type="text" className="form-control shadow-none" id="bookName" required />
+                                        <input type="text" className="form-control shadow-none" name='bookName' id="bookName" required />
                                     </div>
                                     <div className="col-md-4">
                                         <label htmlFor="bookPrice" className="form-label">Price of the book <span className="text-danger">*</span></label>
-                                        <input type="number" className="form-control shadow-none" id="bookPrice" required />
+                                        <input type="number" className="form-control shadow-none" name='bookPrice' id="bookPrice" required />
                                     </div>
                                     <div className="col-md-3">
                                         <label htmlFor="selectBookCategory" className="form-label">Book Type <span className="text-danger">*</span></label>
-                                        <select id="selectBookCategory" className="form-select form-control shadow-none" defaultValue={"Select"}>
+                                        <select id="selectBookCategory" name='bookType' className="form-select form-control shadow-none" defaultValue={"Select"}>
                                             <option disabled>Select</option>
-                                            <option>All</option>
-                                            <option>Books</option>
-                                            <option>Notes</option>
-                                            <option>Solution</option>
-                                            <option>Other Material</option>
+                                            {options.map((option, index)=>{
+                                                return(
+                                                    <option key={index}>
+                                                        {option}
+                                                    </option>
+                                                )
+                                            })}
                                         </select>
                                     </div>
                                     <div className="col-md-5">
                                         <label htmlFor="bookAuthor" className="form-label">Author</label>
-                                        <input type="text" className="form-control shadow-none" id="bookAuthor" />
+                                        <input type="text" name='bookAuthor' className="form-control shadow-none" id="bookAuthor" />
                                     </div>
                                     <div className="col-md-4">
                                         <label htmlFor="bookQuantity" className="form-label">Quantity</label>
-                                        <input type="number" className="form-control shadow-none" id="bookQuantity" />
+                                        <input type="number" name='bookQuantity' className="form-control shadow-none" id="bookQuantity" />
                                     </div>
                                     <div className="col-md-3">
                                         <label htmlFor="bookPhoto" className="form-label">Photo of the book</label>
-                                        <input className="form-control shadow-none" type="file" id="bookPhoto" />
+                                        <input className="form-control shadow-none" name='bookPhoto' type="file" id="bookPhoto" />
                                     </div>
                                     <div className="text-center mt-5">
                                         <button type="submit" className="btn btn-success">Submit</button>
