2c9c1b39f057e6398dbbe479a51e3bf52cdb14c2
RNyaupane <roshan.nyaupane@apexcollege.edu.np>
2023-06-24 22:35:04 +0545

    dashboard data added

--- src/pages/profile/external/ExternalDashboard.js
+++ src/pages/profile/external/ExternalDashboard.js
@@ -1,10 +1,9 @@
 import React, { useState } from 'react'
 import Nav from '../../../common/Nav'
 import './adminDashboard.css'
-import baby from '../../../assets/images/book3.jpg'
 
 
-const AdminDashboard = () => {
+const ExternalDashboard = () => {
     const currentDate = new Date();
     const options = {
         month: '2-digit',
@@ -114,80 +113,6 @@ const AdminDashboard = () => {
                                 </div>
                                 {/* <!-- End Customers Card --> */}
 
-                                {/* Recent Sales */}
-                                <div className="col-12 ">
-                                    <div className="card recent-sales overflow-auto mw-100 rounded-0">
-                                        <h1 className="card-title fs-2 text-center">User Management</h1>
-                                        <div className="filter">
-                                            <a className="icon" href="#" data-bs-toggle="dropdown"><i className="bi bi-three-dots"></i></a>
-                                            <ul className="dropdown-menu dropdown-menu-end dropdown-menu-arrow">
-                                                <li className="dropdown-header text-start">
-                                                    <h6>Filter</h6>
-                                                </li>
-                                                <li><a className="dropdown-item" href="#">Today</a></li>
-                                                <li><a className="dropdown-item" href="#">This Month</a></li>
-                                                <li><a className="dropdown-item" href="#">This Year</a></li>
-                                            </ul>
-                                        </div>
-                                        <div className="card-body  shadow-none collapse1">
-                                            <table className="table table-borderless datatable ">
-                                                <thead className='table-light'>
-                                                    <tr className='border-bottom'>
-                                                        <th scope="col">Id</th>
-                                                        <th scope="col">Username</th>
-                                                        <th scope="col">Date Created</th>
-                                                        <th scope="col">Role</th>
-                                                        <th scope="col">Status</th>
-                                                        <th scope="col">Action</th>
-                                                    </tr>
-                                                </thead>
-                                                <tbody>
-                                                    <tr className='border '>
-                                                        <th scope="row"><a href="#">#01</a></th>
-                                                        <td>roshan34</td>
-                                                        <td><a href="#" className="text-dark">{formattedDate.toString()}</a></td>
-                                                        <td>admin</td>
-                                                        <td><span className="badge bg-success rounded-4 ">active</span></td>
-                                                        <td className=' action-delete text-danger m-0 p-0'>
-                                                            <button className='btn'><i className="bi bi-trash-fill mx-2 text-danger fs-5 rounded-5"></i></button>
-                                                        </td>
-                                                    </tr>
-                                                    <tr className='border '>
-                                                        <th scope="row"><a href="#">#02</a></th>
-                                                        <td>bhawana999</td>
-                                                        <td><a href="#" className="text-dark">{formattedDate.toString()}</a></td>
-                                                        <td>student</td>
-                                                        <td><span className="badge bg-danger rounded-4 ">offline</span></td>
-                                                        <td className=' action-delete text-danger m-0 p-0'>
-                                                            <button className='btn'><i className="bi bi-trash-fill mx-2 text-danger fs-5 rounded-5"></i></button>
-                                                        </td>
-                                                    </tr>
-                                                    <tr className='border '>
-                                                        <th scope="row"><a href="#">#03</a></th>
-                                                        <td>kapil09</td>
-                                                        <td><a href="#" className="text-dark">{formattedDate.toString()}</a></td>
-                                                        <td>acs</td>
-                                                        <td><span className="badge bg-success rounded-4 ">active</span></td>
-                                                        <td className=' action-delete text-danger m-0 p-0'>
-                                                            <button className='btn'><i className="bi bi-trash-fill mx-2 text-danger fs-5 rounded-5"></i></button>
-                                                        </td>
-                                                    </tr>
-                                                    <tr className='border '>
-                                                        <th scope="row"><a href="#">#04</a></th>
-                                                        <td>bhawana999</td>
-                                                        <td><a href="#" className="text-dark">{formattedDate.toString()}</a></td>
-                                                        <td>external</td>
-                                                        <td><span className="badge bg-danger rounded-4 ">offline</span></td>
-                                                        <td className=' action-delete text-danger m-0 p-0'>
-                                                            <button className='btn'><i className="bi bi-trash-fill mx-2 text-danger fs-5 rounded-5"></i></button>
-                                                        </td>
-                                                    </tr>
-                                                </tbody>
-                                            </table>
-                                        </div>
-                                    </div>
-                                </div>
-
                                 {/* Top Selling  */}
                                 <div className="col-12">
                                     <div className="card top-selling overflow-auto mw-100 rounded-0 ">
@@ -272,80 +197,6 @@ const AdminDashboard = () => {
                                         </div>
                                     </div>
                                 </div>
-                                {/* End Top Selling */}
-                                <div className="col-12">
-                                    <div className="card recent-sales overflow-auto mw-100 rounded-0">
-                                        <h1 className="card-title fs-3 text-center">Posts</h1>
-                                        <div className="filter">
-                                            <a className="icon" href="#" data-bs-toggle="dropdown"><i className="bi bi-three-dots"></i></a>
-                                            <ul className="dropdown-menu dropdown-menu-end dropdown-menu-arrow">
-                                                <li className="dropdown-header text-start">
-                                                    <h6>Filter</h6>
-                                                </li>
-                                                <li><a className="dropdown-item" href="#">Today</a></li>
-                                                <li><a className="dropdown-item" href="#">This Month</a></li>
-                                                <li><a className="dropdown-item" href="#">This Year</a></li>
-                                            </ul>
-                                        </div>
-                                        <div className="card-body  shadow-none collapse1">
-                                            <table className="table table-borderless datatable">
-                                                <thead className='table-light '>
-                                                    <tr>
-                                                        <th scope="col">Id</th>
-                                                        <th scope="col">Book Type</th>
-                                                        <th scope="col">Name</th>
-                                                        <th scope="col">Posted Date</th>
-                                                        <th scope="col">Status</th>
-                                                        <th scope="col">Action</th>
-                                                    </tr>
-                                                </thead>
-                                                <tbody>
-                                                    <tr>
-                                                        <th scope="row"><a href="#">#101</a></th>
-                                                        <td>Notes</td>
-                                                        <td><a href="#" className="text-dark">Digital System</a></td>
-                                                        <td><a href="#" className="text-dark">{formattedDate.toString()}</a></td>
-                                                        <td><span className="badge bg-success">Approved</span></td>
-                                                        <td className=' action-delete text-danger m-0 p-0'>
-                                                            <button className='btn'><i className="bi bi-trash-fill mx-2 text-danger fs-5 rounded-5"></i></button>
-                                                        </td>
-                                                    </tr>
-                                                    <tr>
-                                                        <th scope="row"><a href="#">#101</a></th>
-                                                        <td>Notes</td>
-                                                        <td><a href="#" className="text-dark">Digital System</a></td>
-                                                        <td><a href="#" className="text-dark">{formattedDate.toString()}</a></td>
-                                                        <td><span className="badge bg-success">Approved</span></td>
-                                                        <td className=' action-delete text-danger m-0 p-0'>
-                                                            <button className='btn'><i className="bi bi-trash-fill mx-2 text-danger fs-5 rounded-5"></i></button>
-                                                        </td>
-                                                    </tr>
-                                                    <tr>
-                                                        <th scope="row"><a href="#">#101</a></th>
-                                                        <td>Notes</td>
-                                                        <td><a href="#" className="text-dark">Digital System</a></td>
-                                                        <td><a href="#" className="text-dark">{formattedDate.toString()}</a></td>
-                                                        <td><span className="badge bg-success">Approved</span></td>
-                                                        <td className=' action-delete text-danger m-0 p-0'>
-                                                            <button className='btn'><i className="bi bi-trash-fill mx-2 text-danger fs-5 rounded-5"></i></button>
-                                                        </td>
-                                                    </tr>
-                                                    <tr>
-                                                        <th scope="row"><a href="#">#101</a></th>
-                                                        <td>Notes</td>
-                                                        <td><a href="#" className="text-dark">Digital System</a></td>
-                                                        <td><a href="#" className="text-dark">{formattedDate.toString()}</a></td>
-                                                        <td><span className="badge bg-success">Approved</span></td>
-                                                        <td className=' action-delete text-danger m-0 p-0'>
-                                                            <button className='btn'><i className="bi bi-trash-fill mx-2 text-danger fs-5 rounded-5"></i></button>
-                                                        </td>
-                                                    </tr>
-
-                                                </tbody>
-                                            </table>
-                                        </div>
-                                    </div>
-                                </div>
                             </div>
                         </div>
                         {/* End Left side columns  */}
@@ -356,4 +207,4 @@ const AdminDashboard = () => {
     )
 }
 
-export default AdminDashboard
+export default ExternalDashboard
